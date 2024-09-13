4b63c5dcd5c99a438e67e08bbf3479a3206c3846
vndpal <undervinod@gmail.com>
2024-09-13 22:20:10 +0530

    Fix economy rate calculation

--- utils/updatePlayerCareerStats.ts
+++ utils/updatePlayerCareerStats.ts
@@ -16,57 +16,60 @@ export const updatePlayerCareerStats = async (
         (playerCareerStat: playerCareerStats) =>
           playerCareerStat.playerId == playerMatchStat.playerId
       );
+
       if (playerCareerStatIndex > -1) {
-        playerCareerStats[playerCareerStatIndex].matches += 1;
-        playerCareerStats[playerCareerStatIndex].runs += playerMatchStat.runs;
-        playerCareerStats[playerCareerStatIndex].ballsFaced +=
-          playerMatchStat.ballsFaced;
-        playerCareerStats[playerCareerStatIndex].fours += playerMatchStat.fours;
-        playerCareerStats[playerCareerStatIndex].sixes += playerMatchStat.sixes;
-        playerCareerStats[playerCareerStatIndex].notOuts +=
-          playerMatchStat.isOut ? 0 : 1;
-        playerCareerStats[playerCareerStatIndex].strikeRate =
-          (playerCareerStats[playerCareerStatIndex].runs /
-            playerCareerStats[playerCareerStatIndex].ballsFaced) *
-          100;
+        const careerStat = playerCareerStats[playerCareerStatIndex];
 
-        const totalInnings =
-          playerCareerStats[playerCareerStatIndex].matches -
-          playerCareerStats[playerCareerStatIndex].notOuts;
-        playerCareerStats[playerCareerStatIndex].average =
-          playerCareerStats[playerCareerStatIndex].runs /
-          (totalInnings === 0 ? 1 : totalInnings);
+        careerStat.matches += 1;
+        careerStat.runs += playerMatchStat.runs;
+        careerStat.ballsFaced += playerMatchStat.ballsFaced;
+        careerStat.fours += playerMatchStat.fours;
+        careerStat.sixes += playerMatchStat.sixes;
+        careerStat.notOuts += playerMatchStat.isOut ? 0 : 1;
 
-        playerCareerStats[playerCareerStatIndex].innings +=
-          playerMatchStat.ballsFaced > 0 ? 1 : 0;
+        careerStat.strikeRate = (careerStat.runs / careerStat.ballsFaced) * 100;
 
-        playerCareerStats[playerCareerStatIndex].wickets +=
-          playerMatchStat.wickets;
-        playerCareerStats[playerCareerStatIndex].overs += playerMatchStat.overs;
-        playerCareerStats[playerCareerStatIndex].ballsBowled +=
-          playerMatchStat.ballsBowled;
-        if (playerCareerStats[playerCareerStatIndex].ballsBowled > 5) {
-          playerCareerStats[playerCareerStatIndex].overs += 1;
-          playerCareerStats[playerCareerStatIndex].ballsBowled =
-            playerCareerStats[playerCareerStatIndex].ballsBowled % 6;
+        const totalInnings = careerStat.matches - careerStat.notOuts;
+        careerStat.average =
+          careerStat.runs / (totalInnings === 0 ? 1 : totalInnings);
+
+        careerStat.innings += playerMatchStat.ballsFaced > 0 ? 1 : 0;
+
+        careerStat.wickets += playerMatchStat.wickets;
+        careerStat.overs += playerMatchStat.overs;
+        careerStat.ballsBowled += playerMatchStat.ballsBowled;
+
+        if (careerStat.ballsBowled > 5) {
+          careerStat.overs += 1;
+          careerStat.ballsBowled = careerStat.ballsBowled % 6;
         }
-        playerCareerStats[playerCareerStatIndex].extras +=
-          playerMatchStat.extras;
-        playerCareerStats[playerCareerStatIndex].runsConceded +=
-          playerMatchStat.runsConceded;
-        playerCareerStats[playerCareerStatIndex].foursConceded +=
-          playerMatchStat.foursConceded;
-        playerCareerStats[playerCareerStatIndex].sixesConceded +=
-          playerMatchStat.sixesConceded;
-        playerCareerStats[playerCareerStatIndex].maidens +=
-          playerMatchStat.maidens;
-        playerCareerStats[playerCareerStatIndex].bowlingEconomy =
-          playerCareerStats[playerCareerStatIndex].runsConceded /
-          playerCareerStats[playerCareerStatIndex].overs;
-        playerCareerStats[playerCareerStatIndex].dotBalls +=
-          playerMatchStat.dotBalls;
+
+        careerStat.extras += playerMatchStat.extras;
+        careerStat.runsConceded += playerMatchStat.runsConceded;
+        careerStat.foursConceded += playerMatchStat.foursConceded;
+        careerStat.sixesConceded += playerMatchStat.sixesConceded;
+        careerStat.maidens += playerMatchStat.maidens;
+
+        careerStat.bowlingEconomy = calculateBowlingEconomy(
+          careerStat.overs,
+          careerStat.ballsBowled,
+          careerStat.runsConceded
+        );
+
+        careerStat.dotBalls += playerMatchStat.dotBalls;
       }
     });
+
     await setItem(STORAGE_ITEMS.PLAYER_CAREER_STATS, playerCareerStats);
   }
+};
+
+const calculateBowlingEconomy = (
+  overs: number,
+  balls: number,
+  runsConceded: number
+): number => {
+  const oversLeft = overs + balls / 6;
+  const ecom = parseFloat((runsConceded / oversLeft).toFixed(2));
+  return ecom;
 };
